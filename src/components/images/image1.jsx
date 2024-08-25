1cbed9fda2470681230b31ff91ae785659b1d9a3
Reshma0711 <reshmamudedla@gmail.com>
2024-08-25 22:20:41 +0530

    changed altText to alttext

--- src/components/images/image1.jsx
+++ src/components/images/image1.jsx
@@ -2,7 +2,7 @@
 const CustomImage=(prop)=>{
     const {source,height,width,text}=prop
     return (
-        <img src={source} height={height} width={width} altText={text}/>
+        <img src={source} height={height} width={width} alttext={text}/>
     )
 }
 export default CustomImage
