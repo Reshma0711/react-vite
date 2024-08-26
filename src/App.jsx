d9149b76002624b5085e714b1b6834fc269972a3
Reshma0711 <reshmamudedla@gmail.com>
2024-08-26 17:38:10 +0530

    carousel with  navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import './App.css'
 import CustomCard from './components/bootstrap/first-component'
 // import CustomButton from './components/button/button1'
 import CustomreactCard from './components/bootstrap/reactbootstrapcard'
-import CustomCarousel from './components/carousel/custom-carousel'
-import EmployeeData from './data/employeedata'
-
+import UncontrolledExample from './components/carousel/custom-carousel'
+import Header from './components/navbar/navbar'
 
 
 const App=()=>{
  
   
   return (
-  <CustomCarousel/>
+  <>
+
+<Header/>
+  <UncontrolledExample/>
+  </>
+
        
   )
 }
--- src/components/carousel/custom-carousel.jsx
+++ src/components/carousel/custom-carousel.jsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Make sure you have Bootstrap CSS
+import './/customcarousel.css'
+function UncontrolledExample() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.pexels.com/photos/302820/pexels-photo-302820.jpeg?auto=compress&cs=tinysrgb&w=800"
+          alt="First slide"
+          style={{ height: '92vh' }} // Custom height
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.pexels.com/photos/709552/pexels-photo-709552.jpeg?auto=compress&cs=tinysrgb&w=800"
+          alt="Second slide"
+          style={{ height: '92vh' }} // Custom height
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img 
+          className="d-block w-100"
+          src="https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=800"
+          alt="Third slide"
+          style={{ height: '92vh' }} // Custom height
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
 
-const CustomCarousel=()=>{
- 
-        return (
-            <Carousel>
-                <div>
-                    <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=600" height={500} width={500}/>
-                    <p className="legend">Photo 1</p>
-                </div>
-                <div>
-                    <img src="https://images.pexels.com/photos/919278/pexels-photo-919278.jpeg?auto=compress&cs=tinysrgb&w=600" height={500} width={500}/>
-                    <p className="legend">Photo 2</p>
-                </div>
-                <div>
-                    <img src="https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=600" height={500} width={500}/>
-                    <p className="legend">Photo 3</p>
-                </div>
-            </Carousel>
-        );
-    }
-
-export default CustomCarousel
+export default UncontrolledExample;
